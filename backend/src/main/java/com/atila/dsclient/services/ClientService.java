6cef820c210630c0b341a08a9bfba9316511ffd7
atilacapovilla <capovillaatila@gmail.com>
2020-10-21 19:43:08 -0300

    Find Client by id, excetion handling

--- backend/src/main/java/com/atila/dsclient/services/ClientService.java
+++ backend/src/main/java/com/atila/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.atila.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.atila.dsclient.dto.ClientDTO;
 import com.atila.dsclient.entities.Client;
 import com.atila.dsclient.repositories.ClientRepository;
+import com.atila.dsclient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,6 +23,13 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 
 }
