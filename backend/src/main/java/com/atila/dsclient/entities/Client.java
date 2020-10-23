e6d2ede3715b30994403e1a87a7ca1e760a03509
atilacapovilla <capovillaatila@gmail.com>
2020-10-23 17:32:46 -0300

    Auditing data

--- backend/src/main/java/com/atila/dsclient/entities/Client.java
+++ backend/src/main/java/com/atila/dsclient/entities/Client.java
@@ -8,25 +8,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String cpf;
 	private Double income;
-	
-	@Column (name = "birthdate", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+
+	@Column(name = "birthdate", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
-	
+
 	private Integer children;
+
+	@Column(name = "createat", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createAt;
+
+	@Column(name = "updateAt", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
 
 	public Client() {
 	}
@@ -88,6 +96,24 @@ public class Client implements Serializable {
 		this.children = children;
 	}
 
+	public Instant getCreateAt() {
+		return createAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updateAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
